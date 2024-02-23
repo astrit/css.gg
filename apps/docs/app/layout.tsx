945e5429fedb1978d05572bc2caf9aa7ee61f6cb
astrit <am@astrit.co>
2024-02-23 22:24:01 +0100

    Msg → Fri Feb 23 22:24:01 CET 2024

--- apps/docs/app/layout.tsx
+++ apps/docs/app/layout.tsx
@@ -1,8 +1,4 @@
-import "./globals.css";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Turborepo",
@@ -16,7 +12,7 @@ export default function RootLayout({
 }): JSX.Element {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>{children}</body>
     </html>
   );
 }
