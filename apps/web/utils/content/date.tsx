08ee35fdbac884a9f5918cae3beb820292c47421
astrit <am@astrit.co>
2024-03-29 01:46:02 +0100

    init deploy

--- apps/web/utils/content/date.tsx
+++
@@ -1,7 +0,0 @@
-import { format, isThisYear } from "date-fns"
-
-export const formatShortDate = (date: string) => {
-  const _date = new Date(date)
-
-  return isThisYear(_date) ? format(_date, "MMM d") : format(_date, "MMM d, y")
-}
