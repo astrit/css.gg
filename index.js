4106ee8fcb97faa89baaf415317230296871c380
astrit <am@astrit.co>
2024-08-26 23:43:55 +0200

    Msg → Mon Aug 26 23:43:55 CEST 2024

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
-import * as CSS from "./icons/icons.css";
-export { CSS };
-
-import * as SCSS from "./icons/icons.scss";
-export { SCSS };
-
-import * as SVG from "./icons/icons.svg";
-export { SVG };
+export * from "./icons/icons.js";
+export * from "./icons/icons.d.ts";
 
 export * from "./icons/icons.js";
 export * from "./icons/icons.d.ts";
+
+import glyphs from "./glyphs/glyphs.json";
+export { glyphs };
